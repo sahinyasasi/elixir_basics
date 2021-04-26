ec66c651be57302533b184649839bad2f54d5c6a
sahinyasasi <sahinyasasi@example.com>
2021-04-26 11:23:35 +0530

    nested lists with doubledup comprehension

--- lib/cards.ex
+++ lib/cards.ex
@@ -16,14 +16,9 @@ defmodule Cards do
     values = ["ACE", "TWO", "THREE", "FOUR", "FIVE"]
     suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
 
-    cards =
-      for value <- values do
-        for suit <- suits do
-          "#{value} of #{suit}"
-        end
-      end
-
-    List.flatten(cards)
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
